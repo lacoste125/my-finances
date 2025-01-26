31d31c89024b9e0efff796dbf024c78ef37df4a4
miwanski <mariusz.iwanski1@gmail.com>
2025-01-26 15:16:53 +0100

    MYSZ-103: fix for double called requests from useEffect - the temporary fix

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,11 +6,15 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
-root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-);
+root.render(<App/>);
+// TODO: Doo ogarnięcia poprawne zarządzanie useEffect
+// Szczegóły https://stackoverflow.com/questions/72238175/why-useeffect-running-twice-and-how-to-handle-it-well-in-react
+//
+// root.render(
+//     <React.StrictMode>
+//         <App/>
+//     </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
