47d42d9973177ca777e267d2cfdf3dcd51895361
Mariusz <mariusz.iwanski1@gmail.com>
2025-06-12 01:24:07 +0200

    MYSZ-110: changed frontend to vite

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <App/>
 );
-root.render(<App/>);
+// root.render(<App/>);
 // TODO: Doo ogarnięcia poprawne zarządzanie useEffect
 // Szczegóły https://stackoverflow.com/questions/72238175/why-useeffect-running-twice-and-how-to-handle-it-well-in-react
 //
@@ -19,4 +18,3 @@ root.render(<App/>);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
